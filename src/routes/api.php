3d1dba652415cb6761343da460f8510f090b78f0
Fillipe Oliveira <fillipecool@gmail.com>
2026-03-09 15:29:12 -0300

    feat(route): add clients route and change code logic

--- src/routes/api.php
+++ src/routes/api.php
@@ -2,14 +2,27 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\ClientController;
 use Illuminate\Http\Request;
 
+// --------------------
+// Public Routes (Auth)
+// --------------------
 Route::post('/register', [AuthController::class, 'register']);
-
-Route::middleware('auth:sanctum')->get('/me', function (Request $request) {
-    return $request->user();
-});
-
 Route::post('/login', [AuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
+// --------------------
+// Protected Routes
+// --------------------
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::get('/me', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::apiResource('clients', ClientController::class);
+
+    // TODO: Tasks, Notifications, Transactions
+});
